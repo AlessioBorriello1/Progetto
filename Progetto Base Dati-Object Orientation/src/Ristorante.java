6270ce31ba790139ceb59bfe76c0aa98e0a1828f
TheNinjaTuna <TheNinjaTuna@TheNinjaTunasPC.homenet.telecomitalia.it>
2020-06-24 00:24:08 +0200

    Rimpiazzata classe LuogoTipoRistorante con Ristorante

---
+++ Progetto Base Dati-Object Orientation/src/Ristorante.java
@@ -0,0 +1,13 @@
+import javax.swing.JPanel;
+
+public class Ristorante extends Luogo{
+
+	public Ristorante creaRistorante(String nome, String indirizzo, double telefono, String proprietario, String tipoAttivita, String specializzazione, JPanel pannelloImpostazioniAggiuntive) {
+		
+		System.out.println(pannelloImpostazioniAggiuntive.getName());
+		
+		return null;
+		
+	}
+	
+}
